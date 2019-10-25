7fd59dbfc477aafb18ca48359504b9fae62e6ce2
MokkeMeguru <meguru.mokke@gmail.com>
2019-10-25 14:38:42 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
                  [duct/module.logging "0.4.0"]
                  [duct/module.sql "0.5.0"]
                  [duct/module.web "0.7.0"]
-                 [org.postgresql/postgresql "42.2.5"]]
+                 [org.postgresql/postgresql "42.2.5"]
+                 [metosin/compojure-api "2.0.0-alpha30"]]
   :plugins [[duct/lein-duct "0.12.1"]]
   :main ^:skip-aot hello-duct.main
   :resource-paths ["resources" "target/resources"]
--- src/hello_duct/handler/example.clj
+++ src/hello_duct/handler/example.clj
@@ -1,8 +1,28 @@
 (ns hello-duct.handler.example
-  (:require [compojure.core :refer :all]
-            [integrant.core :as ig]))
+  (:require ;; [compojure.core :refer :all]
+   [compojure.api.sweet :refer :all]
+   [clojure.java.io :as io]
+   [integrant.core :as ig]))
 
 (defmethod ig/init-key :hello-duct.handler/example [_ options]
-  (context "/example" []
-    (GET "/" []
-      {:body {:example "data"}})))
+  (api
+   {:swagger
+    {:ui "/api-docs"
+     :spec "/swagger.json"
+     :data {:info {:title "sample API"
+                   :description "compojure api + duct sample"}
+            :tags [{:name "api", :description "sample apis"}]
+            :consumes ["application/json"]
+            :produces ["application/json"]}}}
+   (context "/example" []
+            (GET "/" []
+                 {:body {:example "data"}}))))
+
+;; (ns hello-duct.handler.example
+;;   (:require [compojure.core :refer :all]
+;;             [integrant.core :as ig]))
+
+;; (defmethod ig/init-key :hello-duct.handler/example [_ options]
+;;   (context "/example" []
+;;     (GET "/" []
+;;          {:body {:example "data"}})))
